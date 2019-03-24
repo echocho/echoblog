8a91949ccad1809dcd7c300ae580d0393650b1fa
echocho <echo333cheng@gmail.com>
2019-03-25 00:02:53 +0800

    added urls for about_me, login, logout

--- app/routes.py
+++ app/routes.py
@@ -7,5 +7,15 @@ def index():
 	posts = ['test1', 'test2', 'test3']
 	return render_template('index.html', title='', posts=posts)
 
+@app.route('/about_me', methods=['GET'])
+def about_me():
+	return 'About me page'
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	return 'login page'
+
+@app.route('/logout')
+def logout():
+	return 'logout user'
 
